1e68bb672e0e3b74175fa2fbe35f6c5a17db0877
Mauriange Rachel <mauriange.rachel@gmail.com>
2022-08-08 17:43:56 +0200

    remove scss add axios and button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const Button = () => {
+    const [data, setData] = useState([]);
+
+    const handleClick = async () => {
+
+        const { data } = await axios.get('https://randomuser.me/api/?results=12');
+        console.log('DATA:', data);
+        setData(data);
+    }
+
     return (
         <div>
-            
+            <button onClick={handleClick}>Afficher la liste</button>
         </div>
     );
 };
