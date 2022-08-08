a911c32059008c429ab96bb474da498ae7084ea2
Mauriange Rachel <mauriange.rachel@gmail.com>
2022-08-08 15:59:43 +0200

    starting app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/"/>
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
